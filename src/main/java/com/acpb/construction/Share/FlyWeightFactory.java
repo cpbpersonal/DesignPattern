53b8ad1161641296ba694116313562f02c4b2691
acpb <2210649776@qq.com>
2022-08-11 22:50:14 +0800

    享元模式

--- src/main/java/com/acpb/construction/Share/FlyWeightFactory.java
+++ src/main/java/com/acpb/construction/Share/FlyWeightFactory.java
@@ -3,7 +3,7 @@ package com.acpb.construction.Share;
 import java.util.HashSet;
 import java.util.Set;
 /**
- * @author 22106
+ * @author chengpengbo
  */ //享元对象池，负责维护享元对象。通过池技术 复用相同对象，极大的节约系统资源
 //享元对象的内外状态分离，外部状态客户端指定，内部状态自己维护
 public class FlyWeightFactory {
